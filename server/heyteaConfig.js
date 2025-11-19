fa698efa2a45e43309bd7ae95e94716350d82cda
Jerry Zhou <i@jerryz.com.cn>
2025-11-19 13:44:38 +0800

    Update heyteaConfig.js

--- server/heyteaConfig.js
+++ server/heyteaConfig.js
@@ -1,6 +1,6 @@
 const HEYTEA_API_BASE = "https://app-go.heytea.com";
 const HEYTEA_BASE_HEADERS = {
-  Accept: "application/prs.heytea.v1+json",
+  Accept: "*/*",
   "Content-Type": "application/json",
   "x-client-version": "4.0.1",
   "client-version": "4.0.1",
@@ -10,6 +10,8 @@ const HEYTEA_BASE_HEADERS = {
   "x-client": "app",
   client: "2",
   "x-version": "4.0.1",
+  "charset": "utf-8",
+  "Referer": "https://servicewechat.com/wx696a42df4f2456d3/400000137/page-frame.html",
   "User-Agent":
     "Mozilla/5.0 (Linux; Android 16; 2410DPN6CC Build/BP2A.250605.031.A3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/138.0.7204.180 Mobile Safari/537.36 XWEB/1380283 MMWEBSDK/20250904 MMWEBID/9130 MicroMessenger/8.0.64.2940(0x28004033) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android",
 };
